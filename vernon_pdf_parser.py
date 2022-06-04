705ff202bc2b05c57f45d17ed95547ce5e84c810
aspiazza <apizaz99@gmail.com>
2022-06-04 17:57:27 -0400

    Completed filter of 4 UNIT property blocks

--- vernon_pdf_parser.py
+++ vernon_pdf_parser.py
@@ -1,13 +1,13 @@
 import pdfplumber
 
 
-def vernon_ny_parser(pdf_file, units):
+def vernon_ny_parser(pdf_file, search_param):
     """
     pdf_file: Specify PDF path
-    units: Specify number of units
+    search_param: Specify text to search for in property block
     """
     with pdfplumber.open(pdf_file) as pdf:
-        with open("4_unit.txt", 'w') as txt_file:
+        with open(f"Vernon_{search_param}.txt", 'w') as txt_file:
             pdf_size = int(str(pdf.pages[-1])[6:-1]) - 1
             pdf_count = 0
 
@@ -19,10 +19,10 @@ def vernon_ny_parser(pdf_file, units):
 
                 for prop in property_block[2:-1]:
 
-                    if f"{units} UNITS" in prop:
+                    if f"{search_param}" in prop:
                         txt_file.write(prop)
 
                 pdf_count = pdf_count + 1
 
 
-vernon_ny_parser(pdf_file="2021_Final_Roll_Website_Updated.pdf", units="4")
+vernon_ny_parser(pdf_file="2021_Final_Roll_Website_Updated.pdf", search_param="4 UNITS")
