e877540f24912d525668c50a50d7ca139a44c158
aspiazza <apizaz99@gmail.com>
2022-06-06 22:17:22 -0400

    Refined pdf to excel script

--- pdf_to_excel.py
+++ pdf_to_excel.py
@@ -8,17 +8,18 @@ def vernon_ny_parser(pdf_file):
     """
     pdf_file: Specify PDF path
     """
-    pdf_count = 0
-    row = 2
+    row = 2  # First row reserved for column_list
+    address_abb = [" ST", " AVE", " PL", " AV", " PLACE", " AVENUE", " RD", " ROAD", " DR", " DRIVE", " COURT",
+                   " STREET", " PLAZA", " TERRACE", " LA", " LANE", " LN", " PKY", " PARKWAY", " PKWAY", " NY"]
+    column_list = ["Tax Map Parcel ID", "Account Number", "Property Address", "Apartment", "Number of Units",
+                   "Current Owners Name", "Current Owners Address", "Acerage", "Full Market Value"]
+
     wb = Workbook()
     ws = wb.active
-
-    column_list = ["Property Address", "Account Number", "Tax Map Parcel ID", "Apartment",
-                   "Current Owners Name", "Current Owners Address", "Number of Units"]
     ws.append(column_list)
-
     with pdfplumber.open(pdf_file) as pdf:
         pdf_size = int(str(pdf.pages[-1])[6:-1]) - 1
+        pdf_count = 0
 
         while pdf_count < pdf_size:
             page = pdf.pages[pdf_count]
@@ -27,41 +28,64 @@ def vernon_ny_parser(pdf_file):
                 "**********************************************************************************************")
 
             for property_block in property_blocks[1:-1]:
-                property_block = property_block.replace("**********************", "")
                 property_block = re.split(r'\s{4,}', property_block)
+                property_block = property_block[1:]
 
-                for index, text_line in enumerate(property_block[1:]):
-                    if index == 4 or index == 5 or index == 7 or index == 8 or index == 9:
-                        continue
-                    elif index == 0:
-                        cell_ref = ws.cell(row=row, column=1)
-                        cell_ref.value = text_line
-                    elif index == 1:
-                        cell_ref = ws.cell(row=row, column=2)
-                        cell_ref.value = text_line
-                    elif index == 2:
+                for index, text_line in enumerate(property_block):
+                    if index == 0:  # Property Address
                         cell_ref = ws.cell(row=row, column=3)
                         cell_ref.value = text_line
-                    elif index == 3:
+
+                    elif "ACCT: " in text_line:  # Account Number
+                        cell_ref = ws.cell(row=row, column=2)
+                        text_line = text_line.split(" ")[1]
+                        cell_ref.value = text_line
+
+                    elif index == 2 or "." in text_line and "-" in text_line:  # Parcel ID
+                        cell_ref = ws.cell(row=row, column=1)
+                        cell_ref.value = text_line
+
+                    elif index == 3 and "   " in text_line:  # Apartment Number
                         cell_ref = ws.cell(row=row, column=4)
+                        text_line = text_line.split("   ")[1]
                         cell_ref.value = text_line
-                    elif index == 6:
-                        cell_ref = ws.cell(row=row, column=5)
-                        cell_ref.value = text_line
-                    elif index == 10:
+
+                    elif ", " in text_line:  # Current Owners Name
                         cell_ref = ws.cell(row=row, column=6)
-                        cell_ref.value = text_line
-                    elif "UNITS" in text_line:
+                        if cell_ref.value:
+                            cell_ref.value = (cell_ref.value + "/" + text_line)
+                        else:
+                            cell_ref.value = text_line
+
+                    elif any(abb in text_line for abb in address_abb):  # Owner Address
                         cell_ref = ws.cell(row=row, column=7)
+                        if cell_ref.value:
+                            cell_ref.value = (cell_ref.value + ", " + text_line)
+                        else:
+                            cell_ref.value = text_line
+
+                    elif "UNITS" in text_line:  # Number of Units
+                        cell_ref = ws.cell(row=row, column=5)
+                        text_line = text_line.split(" ")[0]
+                        cell_ref.value = text_line
+
+                    elif "ACREAGE " in text_line:  # Acres
+                        cell_ref = ws.cell(row=row, column=8)
+                        text_line = text_line.split("  ")[1]
+                        cell_ref.value = text_line
+
+                    elif "FULL MKT VAL" in text_line:  # Full Market Value
+                        cell_ref = ws.cell(row=row, column=9)
+                        text_line = text_line.split("  ")[1]
                         cell_ref.value = text_line
                     else:
                         continue
                 row += 1
-
             pdf_count = pdf_count + 1
 
     wb.save("vernon_real_estate_2021.xlsx")
     pdf.close()
+    exit()
 
 
 vernon_ny_parser(pdf_file="2021_Final_Roll_Website_Updated.pdf")
